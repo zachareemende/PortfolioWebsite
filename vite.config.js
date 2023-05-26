148b60aaf6ecfcf34c273f52ab4eb3e979c08655
Zacharee Mende <zachareeriley@gmail.com>
2023-05-25 19:44:29 -0700

    revert

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRefresh()],
+  plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
-  build: {
-    rollupOptions: {
-      // Add any custom Rollup options here
-      external: ["react", "react-dom"],
-    },
-  },
-  optimizeDeps: {
-    include: ["react", "react-dom"],
   },
 });
