41a970148a8d6aaea0073ed9d4fe4217e36f19be
zachareemende <zachareeriley@gmail.com>
2023-06-26 00:29:58 -0800

    adjusted projects order

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,6 +16,7 @@ const Projects = () => {
           <div className="bg-yellow-300 h-1 mt-1 m-px w-1/4 md:w-28 rounded"></div>
         </div>
       </div>
+
       {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
       {/* PORTFOLIO SITE */}
       {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
@@ -69,76 +70,10 @@ const Projects = () => {
           </a>
         </div>
       </div>
-      {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
-      {/* TASK APP */}
-      {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
-      <div className="bg-slate-800 p-6 rounded-lg shadow overflow-hidden text-center">
-        <div>
-          <p className="text-white text-3xl font-bold text-center">taskApp</p>
-          <div className="flex justify-center">
-            <div className="bg-yellow-300 h-1 mb-6 w-1/4 md:w-28 rounded"></div>
-          </div>
-        </div>
-        <a href="https://zachareemendetaskapp.netlify.app/">
-          <img
-            src={TaskAppPic}
-            alt="To-do list / task app ui"
-            className="h-36 md:h-40 mx-auto rounded"
-          />
-        </a>
 
-        <h2 className="mt-4 text-center">
-          <ul className="flex flex-col">
-            <li className="mb-2">- Interactive UI</li>
-            <li className="mb-2">
-              - Task Create, Complete, and Delete function
-            </li>
-            <li className="mb-2">- React / Vite</li>
-            <li className="mb-2">- Bootstrap</li>
-            <li className="mb-2">- PropTypes</li>
-          </ul>
-        </h2>
-
-        <div className="flex justify-end">
-          <a href="https://zachareemendetaskapp.netlify.app/">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-12 m-5"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-              />
-            </svg>
-          </a>
-
-          <a href="https://github.com/zachareemende/taskApp">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-12 m-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z"
-              />
-            </svg>
-          </a>
-        </div>
-      </div>
       {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
       {/* FIREBASE LOGIN REGISTER DEMO */}
       {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
-
       <div className="bg-slate-800 p-6 rounded-lg shadow overflow-hidden text-center">
         <div>
           <p className="text-white text-3xl font-bold text-center">
@@ -205,6 +140,74 @@ const Projects = () => {
           </a>
         </div>
       </div>
+
+      {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
+      {/* TASK APP */}
+      {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
+      <div className="bg-slate-800 p-6 rounded-lg shadow overflow-hidden text-center">
+        <div>
+          <p className="text-white text-3xl font-bold text-center">taskApp</p>
+          <div className="flex justify-center">
+            <div className="bg-yellow-300 h-1 mb-6 w-1/4 md:w-28 rounded"></div>
+          </div>
+        </div>
+        <a href="https://zachareemendetaskapp.netlify.app/">
+          <img
+            src={TaskAppPic}
+            alt="To-do list / task app ui"
+            className="h-36 md:h-40 mx-auto rounded"
+          />
+        </a>
+
+        <h2 className="mt-4 text-center">
+          <ul className="flex flex-col">
+            <li className="mb-2">- Interactive UI</li>
+            <li className="mb-2">
+              - Task Create, Complete, and Delete function
+            </li>
+            <li className="mb-2">- React / Vite</li>
+            <li className="mb-2">- Bootstrap</li>
+            <li className="mb-2">- PropTypes</li>
+          </ul>
+        </h2>
+
+        <div className="flex justify-end">
+          <a href="https://zachareemendetaskapp.netlify.app/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-12 m-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+              />
+            </svg>
+          </a>
+
+          <a href="https://github.com/zachareemende/taskApp">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-12 m-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+      
       {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
       {/* OTHER / GITHUB LINK */}
       {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
